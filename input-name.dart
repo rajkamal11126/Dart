9b508955a0f4be0089bef84c3ff2328433b4101c
Raj Kamal <rajkamal11126@gmail.com>
2022-12-23 14:14:10 +0530

    Input-from-user

---
+++ input-name.dart
@@ -0,0 +1,17 @@
+// importing dart:io file
+import 'dart:io';
+ 
+void main()
+{
+    print("Enter your name?");
+    // Reading name
+    String? name = stdin.readLineSync();
+    print("Enter your department?");
+    //Reading department
+    String? department = stdin.readLineSync();
+ 
+    // Printing the name
+    print("Hello, $name! \nWelcome to Dart!!");
+    //printing the department
+    print("Your department is $department");
+}
