4cfb3f3eaf193b02966613c484cfda11ca2ca638
Raj Kamal <rajkamal11126@gmail.com>
2022-12-23 14:03:52 +0530

    add two numbers

---
+++ add-two-number.dart
@@ -0,0 +1,12 @@
+void main(){
+
+var add = AddTwoNumbers();
+add.addNumbers(11,44);    
+}
+class AddTwoNumbers{
+
+    void addNumbers(int a,int b){
+        // var c = a+b;
+        print('The sum of two $a and $b is ${a+b}');
+    }
+}
